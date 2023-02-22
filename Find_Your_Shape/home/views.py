925538725de40f6d20691c584f317bf748bd6ca1
MikeyRedmon <michaelredmond059@gmail.com>
2023-02-22 21:25:48 +0000

    start adding crud functionality

--- Find_Your_Shape/home/views.py
+++ Find_Your_Shape/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import hiitbook, hittclasses
 
 # Create your views here.
@@ -9,6 +9,13 @@ def home(request):
 
 
 def booking(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        trainer = request.POST.get('item_trainer')
+        focus = request.POST.get('item_focus')
+        time = request.POST.get('item_time')
+        hittclasses.objects.Create(name=name, trainer=trainer, focus=focus, time=time)
+        return redirect('booking')
     hiitbooks = hiitbook.objects.all()
     context = {
         'hiitclasses':  hiitbooks
