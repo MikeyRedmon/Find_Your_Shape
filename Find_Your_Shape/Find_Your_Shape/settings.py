3d3b9939db8474ff1c403bf51493696549b96b29
MikeyRedmon <michaelredmond059@gmail.com>
2023-03-27 16:04:21 +0000

    Prepare for deployment to Heroku

--- Find_Your_Shape/Find_Your_Shape/settings.py
+++ Find_Your_Shape/Find_Your_Shape/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +77,18 @@ WSGI_APPLICATION = 'Find_Your_Shape.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if DEBUG == FALSE:
+    DATABASE_URL = os.environ.get('DATABASE_URL', '')
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+
+    
+
 
 
 # Password validation
