3a0d86aee9de98ccda95b3e1f93a0cb9bb33b3b2
MikeyRedmon <michaelredmond059@gmail.com>
2023-03-28 17:50:46 +0000

    Troubleshooting cloudinary issues

--- Find_Your_Shape/settings.py
+++ Find_Your_Shape/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["michaelredmond-find-your-shape.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["michaelredmond-find-your-shape.herokuapp.com", "localhost", '127.0.0.1']
 
 
 # Application definition
