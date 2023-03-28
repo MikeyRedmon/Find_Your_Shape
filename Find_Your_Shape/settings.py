f0cdc650f82b6294a15ad1a0b579c4caf7ae0873
MikeyRedmon <michaelredmond059@gmail.com>
2023-03-28 15:36:02 +0000

    Added staticfiles to root to push to production

--- Find_Your_Shape/settings.py
+++ Find_Your_Shape/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/images/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
